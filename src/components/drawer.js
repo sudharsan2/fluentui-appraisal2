cd6864b7de9a238154a4bcd32e7e29d1504e5cb1
EswarSudhan <skeswarsudhan@gmail.com>
2024-05-13 15:39:30 +0530

    escom

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useCallback } from "react";
 import {
   DrawerBody,
@@ -5,58 +6,59 @@ import {
   DrawerHeaderTitle,
   Drawer,
   Button,
-  makeStyles,
-  shorthands,
   tokens,
   flatTreeClassNames,
 } from "@fluentui/react-components";
 import { ArrowCircleRightRegular , ArrowCircleLeftRegular, TextColumnWideRegular} from "@fluentui/react-icons";
 import { Avatar } from "@fluentui/react-components";
-
-
+import { makeStyles, shorthands } from '@griffel/react';
+ 
+ 
 const useStyles = makeStyles({
   root: {
     ...shorthands.border("2px", "solid", "#ccc"),
     ...shorthands.overflow("hidden"),
-
-    display: "flex",
+    marginTop:'6vh',
+    position:'fixed',
+    display:"flex",
+    left:'0',
     height: "100%",
     width: "100vw",
     backgroundColor: "#fff",
   },
-
+ 
   content: {
     ...shorthands.margin(tokens.spacingVerticalXL, tokens.spacingHorizontalXL),
     ...shorthands.flex(1),
-
+ 
     gridRowGap: tokens.spacingVerticalXXL,
   },
 });
-
+ 
 export const Responsive = () => {
   const styles = useStyles();
-
+ 
   const [isOpen, setIsOpen] = useState(true);
   const [change, setChange] = useState(false);
-  const [resize, setresize] = useState(5);
+  const [resize, setresize] = useState(60);
   const [type, setType] = useState("inline");
   
-
-
+ 
+ 
   const onMediaQueryChange = useCallback(({ matches }) => setType(matches ? "overlay" : "inline"), []);
-
+ 
   useEffect(() => {
     const match = window.matchMedia("(max-width: 720px)");
-
+ 
     if (match.matches) {
       setType("overlay");
     }
-
+ 
     match.addEventListener("change", onMediaQueryChange);
-
+ 
     return () => match.removeEventListener("change", onMediaQueryChange);
   }, []);
-
+ 
   return (
     <div className={styles.root} style={{}}>
       <Drawer
@@ -65,8 +67,23 @@ export const Responsive = () => {
         position="start"
         open={true}
         onOpenChange={(_, { open }) => setIsOpen(open)}
-        style={change ? { width: `${resize}vw`, transition: "width 0.5s", backgroundColor:"#E9E9E9" } : {transition: "width 0.5s", backgroundColor:"#E9E9E9"}}
+        style={change ? { width: `${resize}px`, backgroundColor:"#E9E9E9" } : {backgroundColor:"#E9E9E9" }}
       >
+        <div style={change?{display:"flex", justifyContent:"center", alignItems:"center", marginLeft:"10px"}:{display:"flex", alignItems:"center", marginLeft:"0.75vw", width:"100%", cursor:'pointer'}} onClick={() => {
+                          setChange(!change);
+                        }}>
+            <Button
+                        appearance="transparent"
+                        icon={<TextColumnWideRegular />}
+                        size="large"
+                        onClick={() => {
+                          setChange(!change);
+                        }}
+                        style={{backgroundColor:"#E9E9E9", marginTop:"3vh"}}
+                      >
+                        
+                      </Button>
+          </div>
         
         <DrawerHeader style={ {display:"flex"}}>
           <DrawerHeaderTitle
@@ -80,27 +97,15 @@ export const Responsive = () => {
             // }
           >
             
-            
-            <Button
-                        appearance="transparent"
-                        icon={<TextColumnWideRegular />}
-                        size="large"
-                        onClick={() => {
-                          setChange(!change);
-                        }}
-                        style={{backgroundColor:"#E9E9E9"}}
-                      >
-                        
-                      </Button>
-
+             
           </DrawerHeaderTitle>
         </DrawerHeader>
-
+ 
         <DrawerBody style={{backgroundColor:"#E9E9E9"}}>
           {/* <p>Drawer content</p> */}
         </DrawerBody>
       </Drawer>
-
+ 
       <div className={styles.content} >
         {/* <Button appearance="primary" onClick={() => setIsOpen(!isOpen)}>
           Toggle
@@ -108,12 +113,12 @@ export const Responsive = () => {
         <Button appearance="primary" >
             Resize
         </Button>
-
-
+ 
+ 
         <p>Resize the window to see the change</p> */}
       </div>
     </div>
   );
 };
-
+ 
 export default Responsive;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
@@ -7,7 +7,12 @@ import App from './App';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
+  
   <FluentProvider theme={webLightTheme} style={{height: "100vh", width: "100vw"}}>
+  
+    
     <App />
-  </FluentProvider>,
+   
+  </FluentProvider>
+  
 );
